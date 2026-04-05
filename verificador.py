0928d122197588229b760d746cf1b6b0216b8f67
juanp220077 <juanp220077@gmail.com>
2026-04-04 23:28:29 -0500

    Mi primer script de auditoría en Python

---
+++ verificador.py
@@ -0,0 +1,8 @@
+import os
+from datetime import datetime
+
+print(f"--- Auditoría de Seguridad de Juan Puente ---")
+print(f"Fecha y hora actual: {datetime.now()}")
+print(f"Directorio actual: {os.getcwd()}")
+print("Estado del blindaje: Activo")
+
